e889ed9260d575f1cef291e7882da2360ab68822
mahasivan <sivanperuman@gmail.com>
2024-04-18 21:11:40 +0300

    updated nostra

--- products.js
+++ products.js
@@ -2,79 +2,79 @@ const products = [
     {
         id:1,
         name:"Floral Summer Shirt",
-        src:"products/f1.jpg",
+        src:"./img/products/n2.jpeg",
         desc:"",
-        price:220,
+        price:20,
         tags:["new","blue","summer"]
     },
 
     {
         id:2,
         name:"Summer Green",
-        src:"products/f2.jpg",
+        src:"./img/products/f2.jpg",
         desc:"",
-        price:260,
+        price:30,
         tags:["new","green","beach"]
     },
 
     {
         id:3,
         name:"Party Floral Shirt",
-        src:"products/f3.jpg",
+        src:"./img/products/f1.jpg",
         desc:"",
-        price:399,
+        price:25,
         tags:["old","red","party"]
     },
 
     {
         id:4,
-        name:"Floral Summer Shirt",
-        src:"products/f4.jpg",
+        name:"Summer Vibes",
+        src:"./img/products/n77.jpeg",
         desc:"",
-        price:399,
+        price:40,
         tags:["old","white","beach"]
     },
     {
         id:5,
-        name:"Beach Shirt",
-        src:"products/f5.jpg",
+        name:"Shirt Party",
+        src:"./img/products/f7.jpg",
         desc:"",
-        price:579,
+        price:27,
         tags:["old","white","beach"]
     }
 ,
     {
         id:6,
-        name:"Shirt Party Red",
-        src:"products/f6.jpg",
+        name:"Party Floral shirt",
+        src:"./img/products/f2.jpg",
         desc:"",
-        price:579,
+        price:19,
         tags:["old","red","party"]
     },
 
     {
         id:7,
-        name:"Party Floral Shirt",
-        src:"products/f3.jpg",
+        name:"Summer Shirt",
+        src:"./img/products/f3.jpg",
         desc:"",
-        price:399,
+        price:15,
         tags:["old","red","party"]
     },
     {
         id:8,
-        name:"Shirt Party Red",
-        src:"products/f6.jpg",
+        name:"Floral Shirt",
+        src:"./img/products/f4.jpg",
         desc:"",
-        price:579,
+        price:25,
         tags:["old","red","party"]
     },
     {
         id:9,
-        name:"Floral Summer Shirt",
-        src:"products/f1.jpg",
+        name:"Summer Shirt Blue",
+        src:"./img/products/f5.jpg",
         desc:"",
-        price:220,
-        tags:["new","blue","summer"]
+        price:32,
+        tags:["new","blue,summer"]
     },
 
 
